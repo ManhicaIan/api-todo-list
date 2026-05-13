1753f528c2bcd5dbd28bd05137e7f758ba0eb89c
Ian Manhica <ianmanhica4@gmail.com>
2026-05-13 21:37:38 +0200

    Initial commit

---
+++ src/test/java/com/manhica/api_todo_list/ApiTodoListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manhica.api_todo_list;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiTodoListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
